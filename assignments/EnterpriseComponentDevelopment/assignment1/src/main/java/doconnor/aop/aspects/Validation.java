06d9b218bcac0e25a5ba37ec1e1305aba7659828
Michael Dreeling <mdreeling@gmail.com>
2012-02-22 16:36:56 -0500

    Some cleanup and commenting.

--- assignments/EnterpriseComponentDevelopment/assignment1/src/main/java/doconnor/aop/aspects/Validation.java
+++ assignments/EnterpriseComponentDevelopment/assignment1/src/main/java/doconnor/aop/aspects/Validation.java
@@ -38,6 +38,9 @@ public class Validation {
 
 	private final Log logger = LogFactory.getLog(getClass());
 
+	/**
+	 * Load errors from properties file.
+	 */
 	@PostConstruct
 	public void loaderrors() {
 		try {
@@ -47,6 +50,10 @@ public class Validation {
 		}
 	}
 
+	/**
+	 * These are pointcuts which cross all 'modification' actions such as adding
+	 * Players, signing etc.
+	 */
 	@Pointcut("execution(* doconnor.aop.service.LeagueManagerImpl.add*(..)) || "
 			+ "execution(* doconnor.aop.service.LeagueManagerImpl.remove*(..)) || "
 			+ "execution(* doconnor.aop.service.LeagueManagerImpl.sign*(..)) || "
@@ -55,13 +62,29 @@ public class Validation {
 	public void modify() {
 	}
 
+	/**
+	 * The is the method which is going to act on each point cut when its
+	 * intercepted.
+	 *
+	 * @param joinPoint
+	 * @throws Throwable
+	 */
 	@Around("modify()")
 	public void validate(ProceedingJoinPoint joinPoint) throws Throwable {
 
+		/**
+		 * A list of all of the errors.
+		 */
 		final List<Errors> errors = new ArrayList<Errors>();
 
+		/**
+		 * For each argument passed into the intercepted method.
+		 */
 		for (Object arg : joinPoint.getArgs()) {
-			validate(errors, arg);
+			/**
+			 * Validate the argument.
+			 */
+			validateArgument(errors, arg);
 		}
 
 		if (errors.size() > 0) {
@@ -76,27 +99,72 @@ public class Validation {
 		logger.debug("Validation succeeded.");
 	}
 
-	private Errors validateWithErrorTagging(final Object arg,
-			final Validator validator, final List<Errors> errors) {
+	/**
+	 * The main validation method. This validates an object and tags it with any
+	 * errors.
+	 *
+	 * @param arg
+	 * @param validator
+	 * @param errors
+	 * @return
+	 */
+	private void validateClassWithErrorTagging(final Object arg,
+			final Validator validator, final List<Errors> errors)
+			throws Exception {
 		final BindException objErrors = new BindException(arg, arg.getClass()
 				.getName());
 		validator.validate(arg, objErrors);
 		if (objErrors.hasErrors()) {
 			errors.add(objErrors);
 		}
-		return objErrors;
 	}
 
-	private void validate(final List<Errors> errors, final Object arg)
+	/**
+	 * Takes a single argument and validates it. Also introspect's the object
+	 * and digs deeper if necessary.
+	 *
+	 * @param errors
+	 * @param arg
+	 * @throws Exception
+	 */
+	private void validateArgument(final List<Errors> errors, final Object arg)
 			throws Exception {
 		for (final Validator validator : getValidators()) {
 			if (validator.supports(arg.getClass())) {
-				logger.debug(validator.getClass() + " can validate "
+				logger.debug("[DOMAIN] " + validator.getClass()
+						+ " can validate "
 						+ arg.getClass());
-				validateWithErrorTagging(arg, validator, errors);
-				checkArgumentForOtherObjects(arg, errors);
+				validateClass(errors, arg, validator);
+			} else {
+				// logger.debug("%% "
+				// + arg.getClass()
+				// +
+				// " is not a known domain Object and as such is not currently validatable");
 			}
 		}
+	}
+
+	/**
+	 * Deeply validates the specified class using introspection.
+	 *
+	 * It firstly does a top level validation, and then digs in.
+	 *
+	 * @param errors
+	 * @param arg
+	 * @param validator
+	 * @throws Exception
+	 */
+	private void validateClass(final List<Errors> errors, final Object arg,
+			final Validator validator) throws Exception {
+		/**
+		 * 1. Validate the class using the found validator.
+		 */
+		validateClassWithErrorTagging(arg, validator, errors);
+		/**
+		 * 2. Introspect the object and recursively call validateClass - tagging
+		 * as you go.
+		 */
+		introspectArgument(arg, errors);
 	}
 
 	/**
@@ -106,7 +174,7 @@ public class Validation {
 	 * @param errors
 	 * @throws Exception
 	 */
-	private void checkArgumentForOtherObjects(final Object arg,
+	private void introspectArgument(final Object arg,
 			final List<Errors> errors) throws Exception {
 
 		final PropertyDescriptor[] propertyDescriptors = PropertyUtils
@@ -116,37 +184,34 @@ public class Validation {
 					.invoke(arg);
 			if ((propertyValue != null)
 					&& (isClassCollection(propertyValue.getClass()))) {
-
-				for (final Object propertyElementValue : ((Collection) propertyValue)) {
-					for (final Validator validator : getValidators()) {
-						if (validator.supports(propertyElementValue.getClass())) {
-							logger.debug("** " + validator.getClass()
-									+ " can validate the Collection "
-									+ propertyElementValue.getClass());
-							validateWithErrorTagging(propertyElementValue,
-									validator, errors);
-							checkArgumentForOtherObjects(propertyElementValue,
-									errors);
-						} else {
-							logger.debug("$$ "
-									+ propertyValue.getClass()
-									+ " is not validatable as a Collection class right now");
-						}
-					}
-				}
+				validateCollectionArgument(errors, propertyValue);
 			} else if (propertyValue != null) {
-				for (final Validator validator : getValidators()) {
-					if (validator.supports(propertyValue.getClass())) {
-						logger.debug(validator.getClass() + " can validate "
-								+ propertyValue.getClass());
-						validateWithErrorTagging(propertyValue, validator,
-								errors);
-						checkArgumentForOtherObjects(propertyValue, errors);
-					} else {
-						logger.debug("%% "
-								+ propertyValue.getClass()
-								+ " is not a Collection or a known domain Object and as such is not currently validatable");
-					}
+				validateArgument(errors, propertyValue);
+			}
+		}
+	}
+
+	/**
+	 * Validates a Collection argument which requires an additional outer loop
+	 * not present in validateArgument.
+	 *
+	 * @param errors
+	 * @param propertyValue
+	 * @throws Exception
+	 */
+	private void validateCollectionArgument(final List<Errors> errors,
+			final Object propertyValue) throws Exception {
+		for (final Object propertyElementValue : ((Collection<?>) propertyValue)) {
+			for (final Validator validator : getValidators()) {
+				if (validator.supports(propertyElementValue.getClass())) {
+					logger.debug("[COLLECTION] " + validator.getClass()
+							+ " can validate the Collection "
+							+ propertyElementValue.getClass());
+					validateClass(errors, propertyElementValue, validator);
+				} else {
+					// logger.debug("$$ "
+					// + propertyValue.getClass()
+					// + " is not validatable as a Collection class right now");
 				}
 			}
 		}
@@ -158,7 +223,7 @@ public class Validation {
 	 * @param c
 	 * @return
 	 */
-	public static boolean isClassCollection(Class c) {
+	public static boolean isClassCollection(Class<? extends Object> c) {
 		return Collection.class.isAssignableFrom(c)
 				|| Map.class.isAssignableFrom(c);
 	}
